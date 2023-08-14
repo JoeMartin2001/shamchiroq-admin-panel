c0159a79c1bc1878cc0977fe2808a7399ec49563
Sardorbek <sardorbekaminjonov2001@mail.ru>
2023-08-14 15:04:14 +0500

    Update BASE_URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,7 @@
 // export const BASE_URL = 'http://194.233.77.183:3001';
 // export const BASE_URL = 'https://my-shamchiroq.uz';
-export const BASE_URL = "http://localhost:3002";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://my-shamchiroq.uz"
+    : "http://localhost:3002";
 export const BASE_URL_API = `${BASE_URL}/api`;
